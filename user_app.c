b780ea254f985ae2fda591fc96d27777c7939f2d
RedLinen <77304008+RedLinen@users.noreply.github.com>
2021-02-02 15:22:38 -0700

    Add Files

---
+++ user_app.c
@@ -0,0 +1,135 @@
+/*!*********************************************************************************************************************
+@file user_app.c                                                                
+@brief User's tasks / applications are written here.  This description
+should be replaced by something specific to the task.
+
+------------------------------------------------------------------------------------------------------------------------
+GLOBALS
+- NONE
+
+CONSTANTS
+- NONE
+
+TYPES
+- NONE
+
+PUBLIC FUNCTIONS
+- NONE
+
+PROTECTED FUNCTIONS
+- void UserApp1Initialize(void)
+- void UserApp1Run(void)
+
+
+**********************************************************************************************************************/
+
+#include "configuration.h"
+
+/***********************************************************************************************************************
+Global variable definitions with scope across entire project.
+All Global variable names shall start with "G_<type>UserApp1"
+***********************************************************************************************************************/
+/* New variables */
+volatile u8 G_u8UserAppFlags;                             /*!< @brief Global state flags */
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Existing variables (defined in other files -- should all contain the "extern" keyword) */
+extern volatile u32 G_u32SystemTime1ms;                   /*!< @brief From main.c */
+extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.c */
+extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
+
+
+/***********************************************************************************************************************
+Global variable definitions with scope limited to this local application.
+Variable names shall start with "UserApp_<type>" and be declared as static.
+***********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+Function Definitions
+**********************************************************************************************************************/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*! @publicsection */                                                                                            
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*! @protectedsection */                                                                                            
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/*!--------------------------------------------------------------------------------------------------------------------
+@fn void UserAppInitialize(void)
+
+@brief
+Initializes the application's variables.
+
+Should only be called once in main init section.
+
+Requires:
+- NONE
+
+Promises:
+- NONE
+
+*/
+void UserAppInitialize(void)
+{
+
+
+} /* end UserAppInitialize() */
+
+  
+/*!----------------------------------------------------------------------------------------------------------------------
+@fn void UserAppRun(void)
+
+@brief Application code that runs once per system loop
+
+Requires:
+- 
+
+Promises:
+- 
+
+*/
+void UserAppRun(void)
+{
+    u32 u32CounterForOuterLoop = 0x00; //initialize counters
+    u32 u32CounterForInnerLoop = 0x00;
+    
+   
+    while(u32CounterForOuterLoop < 63)   //LATA is incremented in this "while" loop
+    {                                    //to make the attached LEDs turn on and off 
+                                             //in a binary counting pattern
+        LATA = LATA ++;                  
+        u32CounterForOuterLoop ++;                                
+            
+            
+        while(u32CounterForInnerLoop < 225000) //This inner "while" loop 
+        {                                      //just burns off time
+                    
+            u32CounterForInnerLoop ++;
+                    
+        }/*End inner 'while' loop */
+
+            
+        u32CounterForInnerLoop = 0x00; /*Reset the counter for the inner loop*/
+    } /*End outer 'while' loop */
+    LATA ^= 0x3F; /*LATA is effectively reset to 0x80 here.*/
+
+    
+} /* end UserAppRun */
+
+
+
+/*------------------------------------------------------------------------------------------------------------------*/
+/*! @privatesection */                                                                                            
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* End of File                                                                                                        */
+/*--------------------------------------------------------------------------------------------------------------------*/
